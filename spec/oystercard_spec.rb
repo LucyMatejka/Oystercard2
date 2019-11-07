27df8fdb1af66f1a1f1e391aa6e1353d03f299b4
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-07 15:19:58 +0000

    #5 top up oystercard balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,4 +15,14 @@ describe Oystercard do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq(0)
   end
+
+  it 'should respond to top up with a parameter' do
+    oystercard = Oystercard.new
+    expect(oystercard).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'should add a given amount to balance' do
+    oystercard = Oystercard.new
+   expect{oystercard.top_up 1}.to change{oystercard.balance}.by 1
+  end
 end
